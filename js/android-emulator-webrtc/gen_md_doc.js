5931e6fef743115ca07c2cee5b30e8aea4d0f75b
Erwin Jansen <jansene@google.com>
2020-07-15 14:05:02 -0700

    Update licenses in source/
    Several source files were missing a license header.

--- js/android-emulator-webrtc/gen_md_doc.js
+++ js/android-emulator-webrtc/gen_md_doc.js
@@ -1,25 +1,40 @@
-const path = require('path');
-const fs = require('fs');
-const reactDocgen = require('react-docgen');
-const ReactDocGenMarkdownRenderer = require('react-docgen-markdown-renderer');
-const componentPath = path.join(__dirname, 'src/components/emulator/emulator.js');
-const renderer = new ReactDocGenMarkdownRenderer(/* constructor options object */);
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const path = require("path");
+const fs = require("fs");
+const reactDocgen = require("react-docgen");
+const ReactDocGenMarkdownRenderer = require("react-docgen-markdown-renderer");
+const componentPath = path.join(
+  __dirname,
+  "src/components/emulator/emulator.js"
+);
+const renderer = new ReactDocGenMarkdownRenderer();
 
 fs.readFile(componentPath, (error, content) => {
-  const documentationPath = path.basename(componentPath, path.extname(componentPath)) + renderer.extension;
-  console.log(documentationPath)
+  const documentationPath =
+    path.basename(componentPath, path.extname(componentPath)) +
+    renderer.extension;
+  console.log(documentationPath);
 
   const doc = reactDocgen.parse(content);
-  const md = renderer.render(
-    /* The path to the component, used for linking to the file. */
-    componentPath,
-    /* The actual react-docgen AST */
-    doc,
-    /* Array of component ASTs that this component composes*/
-    []);
-  fs.writeFile(documentationPath, md, err => {
+  const md = renderer.render(componentPath, doc, []);
+  fs.writeFile(documentationPath, md, (err) => {
     if (err) {
-      console.error(err)
-      return
-  }});
+      console.error(err);
+      return;
+    }
+  });
 });
